fc6d8d77efb954ad6ca2c45789cd2f82d421b282
Josh Seckman <joshseckman@Joshs-MBP.attlocal.net>
2020-10-27 19:55:59 -0400

    treats controller change

--- controllers/treats/index.js
+++ controllers/treats/index.js
@@ -1,15 +1,11 @@
-const Treats = require("../../models/treats")
-const { Router } = require("express")
-const router = Router()
+const Treats = require("../../models/treats");
+const { Router } = require("express");
+const router = Router();
 
 //index route
-router.get("/", async (req,res)=> {
-    res.json(await Treats.find({}))
-})
-
-
-
-
+router.get("/", async (req, res) => {
+  res.json(await Treats.find({}));
+});
 
 //EXPORT ROUTER
-module.exports = router
+module.exports = router;
