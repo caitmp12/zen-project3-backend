ad6086002fc79ebba77a614b3f15eb7bbfff0160
Josh Seckman <joshseckman@Joshs-MBP.attlocal.net>
2020-10-27 12:20:23 -0400

    created schemas for auth, drinks, movies, treats in models/schema

--- models/treats/index.js
+++ models/treats/index.js
@@ -1,14 +1,16 @@
 //Import
-// const { Schema, model } = require("mongoose");
+const { Schema, model } = require("mongoose");
 
 //Treats Schema
 
+const treatSchema = new Schema({
+  name: { type: String, required: true },
+  img: { type: String, required: true },
+  ingredients: [{ type: String }],
+  directions: { type: String },
+});
 
-
-
+const Treat = model("treat", treatSchema);
 
 // EXPORT MODEL
-module.exports = User;
-
-
-
+module.exports = Treat;
