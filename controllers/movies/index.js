24e654f6133b699f4e61d6f8c85437ae0eea9052
Josh Seckman <joshseckman@Joshs-MBP.attlocal.net>
2020-10-27 20:20:18 -0400

    changed movie link

--- controllers/movies/index.js
+++ controllers/movies/index.js
@@ -1,25 +1,25 @@
-const Movies = require("../../models/drinks")
-const { Router } = require("express")
-const router = Router()
+const Movies = require("../../models/movies");
+const { Router } = require("express");
+const router = Router();
 
 //index route
-router.get("/", async (req,res)=> {
-    res.json(await Movies.find({}))
-})
+router.get("/", async (req, res) => {
+  res.json(await Movies.find({}));
+});
 
 //create route
 router.post("/", async (req, res) => {
-    res.json(await Movies.create(req.body));
-  });
+  res.json(await Movies.create(req.body));
+});
 
 //update route
 router.put("/:_id", async (req, res) => {
-    res.json(await Movies.findByIdAndUpdate(req.params._id, req.body));
-  });
+  res.json(await Movies.findByIdAndUpdate(req.params._id, req.body));
+});
 
 //delete route
 router.delete("/:_id", async (req, res) => {
-    res.json(await Movies.findByIdAndRemove(req.params._id));
-  });
+  res.json(await Movies.findByIdAndRemove(req.params._id));
+});
 //EXPORT ROUTER
-module.exports = router
+module.exports = router;
